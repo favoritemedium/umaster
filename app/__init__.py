d1dff7f6282b2d4b77fcee4be06034f21c410b94
Anders McCarthy <ders@not.real>
2015-02-17 16:25:39 +0900

    add sslify

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sslify import SSLify
 from flask.ext.session import Session
+import os
 
 app = Flask(__name__)
+if 'DYNO' in os.environ:
+    sslify = SSLify(app)  # force ssl on heroku
+
 app.config.from_object('config')
 Session(app)
 from app import views
