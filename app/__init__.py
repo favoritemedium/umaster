c1435ccc7a150d1f3f63299d9533965e10a37fa4
Anders McCarthy <ders@not.real>
2015-02-21 15:51:26 +0900

    swap out sslify for proxyfix

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from flask_sslify import SSLify
 from flask.ext.session import Session
-import os
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-if 'DYNO' in os.environ:
-    sslify = SSLify(app)  # force ssl on heroku
-
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.from_object('config')
 Session(app)
+
 from app import views
